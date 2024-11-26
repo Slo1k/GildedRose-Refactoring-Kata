5fb1aeea058673be19cdde929825b0d1bed0e58d
Mikołaj Krakowiak <mikolajkrakowiak666@gmail.com>
2024-11-26 17:07:10 +0100

    Merge conditions for "Backstage passes"

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -30,11 +30,11 @@ export class GildedRose {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
           if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11 && item.quality < 50) {
-              item.quality = item.quality + 1;
+            if (item.sellIn < 11) {
+              item.quality = Math.min(50, item.quality + 1);
             }
-            if (item.sellIn < 6 && item.quality < 50) {
-              item.quality = item.quality + 1;
+            if (item.sellIn < 6) {
+              item.quality = Math.min(50, item.quality + 1);
             }
           }
         }
