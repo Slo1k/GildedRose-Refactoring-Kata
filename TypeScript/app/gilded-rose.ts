3e98c1313ba99878126693e97a30f1411b791e42
Mikołaj Krakowiak <mikolajkrakowiak666@gmail.com>
2024-11-26 17:06:35 +0100

    simplify nested if condition

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,13 +21,11 @@ export class GildedRose {
     for (const item of this.items) {
       if (
         item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert"
+        item.name != "Backstage passes to a TAFKAL80ETC concert" &&
+        item.name != "Sulfuras, Hand of Ragnaros" &&
+        item.quality > 0
       ) {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
+        item.quality -= 1;
       } else {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
