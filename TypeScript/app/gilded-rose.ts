8a05ed27f0af94fcbcfdbda5ce1c954c51f7ce12
Mikołaj Krakowiak <mikolajkrakowiak666@gmail.com>
2024-11-26 17:07:45 +0100

    avoid repeating item.quality < 50

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -28,14 +28,10 @@ export class GildedRose {
         item.quality -= 1;
       } else {
         if (item.quality < 50) {
-          item.quality = item.quality + 1;
+          item.quality += 1;
           if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              item.quality = Math.min(50, item.quality + 1);
-            }
-            if (item.sellIn < 6) {
-              item.quality = Math.min(50, item.quality + 1);
-            }
+            if (item.sellIn < 11) item.quality = Math.min(50, item.quality + 1);
+            if (item.sellIn < 6) item.quality = Math.min(50, item.quality + 1);
           }
         }
       }
