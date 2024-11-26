24afd3349f50cf827053a791ece599f7a4ec8511
Mikołaj Krakowiak <mikolajkrakowiak666@gmail.com>
2024-11-26 17:08:14 +0100

    extract the sellIn decrement

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -36,7 +36,7 @@ export class GildedRose {
         }
       }
       if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn = item.sellIn - 1;
+        item.sellIn -= 1;
       }
       if (item.sellIn < 0) {
         if (item.name != "Aged Brie") {
